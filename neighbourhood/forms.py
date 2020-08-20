5ac1521d143a51384def208f24ed75e7cecf09b7
Grishon Nganga <52824774+GrishonNganga@users.noreply.github.com>
2020-08-20 16:55:24 +0300

    Add register route working with error shoeing login page

--- neighbourhood/forms.py
+++ neighbourhood/forms.py
@@ -6,7 +6,6 @@ class UserRegistrationForm(UserCreationForm):
         model = User
         fields = ['username', 'email', 'password1', 'password2']
 
-
 class UserLoginForm(UserCreationForm):
     class Meta:
         model = User
--- neighbourhood/views.py
+++ neighbourhood/views.py
@@ -1,10 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import UserRegistrationForm 
 
 
 def register(request):
     registerform = UserRegistrationForm()
-
+    
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/login')
+                    
     return render(request, "registration_form.html", {'register_form': registerform})
 
 
