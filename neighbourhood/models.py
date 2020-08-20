e5fe8db41584a7377da5034bcd11a1edd6742f83
Grishon Nganga <52824774+GrishonNganga@users.noreply.github.com>
2020-08-20 11:13:39 +0300

    Initial db backend work.

--- neighbourhood/models.py
+++ neighbourhood/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Hood(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    population = models.CharField(max_length=100)
+    #admin = models.ManyToManyField() Replace or include this after research
+
+class Business(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    hood = models.ForeignKey(Hood, on_delete=models.CASCADE)
+    #user = models.ForeignKey(User, on_delete=models.CASCADE) Include this after figuring how auth will be done.
+    
