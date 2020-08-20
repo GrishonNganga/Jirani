92a43c4cfba67e2cf0d8cc6e6aad23f8aff1da42
Makena-Maryann <maryann.makena00@gmail.com>
2020-08-20 21:45:10 +0300

    Create a form to add a business

--- neighbourhood/forms.py
+++ neighbourhood/forms.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.forms import UserCreationForm
-from .models import User
+from .models import User,Business
+from django import forms
 
 class UserRegistrationForm(UserCreationForm):
     class Meta:
@@ -11,3 +12,8 @@ class UserLoginForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'password']
+
+class AddBizForm(forms.ModelForm):
+    class Meta:
+        model = Business
+        fields = ['name','email','description']        
