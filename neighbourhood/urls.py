f1aba6efe8faa1686f3b15daa54406e600682d81
Muia23 <wilfred.muema@gmail.com>
2020-08-20 19:27:59 +0300

    add views to details pages

--- neighbourhood/urls.py
+++ neighbourhood/urls.py
@@ -4,7 +4,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    url(r'^$', views.home, name='home'),
     url(r'login$', views.login, name='login'),
     url(r'register$', views.register, name='register'),
-    url(r'^$', views.home, name='home'),
+    url(r'announce/', views.announcement, name='announce'),
+    url(r'blog/', views.blog, name='blog'),
+    url(r'business/', views.business, name='business'),
+    url(r'essential/', views.essential, name='essential'),
+    url(r'meeting/', views.meeting, name='meeting'),
+    url(r'profile/', views.profile, name='profile'),
 ]
--- neighbourhood/views.py
+++ neighbourhood/views.py
@@ -16,3 +16,33 @@ def login(request):
 def home(request):
     
     return render(request, "index.html")
+
+#Profile page
+def profile(request):
+    
+    return render(request, "profile.html")
+
+#Announcement page
+def announcement(request):
+    
+    return render(request, "announcement.html")
+
+#Blog page
+def blog(request):
+    
+    return render(request, "blog.html")
+
+#Business page
+def business(request):
+    
+    return render(request, "business.html")
+
+#Essential page
+def essential(request):
+    
+    return render(request, "essential.html")
+
+#Meeting page
+def meeting(request):
+    
+    return render(request, "meeting.html")
